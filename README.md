aea23b4724df49fce71954259910cc9d432f2e5c
Dewald <brddew8@protonmail.com>
2024-04-08 20:04:02 +0200

    Fixed mismatched links

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 - Created using React & [Vite](https://vitejs.dev/) 
 - Based off the [SCRIMBA React course](https://scrimba.com/) challenge
+
+# How to use:
+
+- Clone the repository from github
+- In the console, execute `npm install` to get the required dependencies
+- Execute `npm run dev` to start a live server
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ import gitHubIcon from '../assets/github-rounded.png';
 export default function Footer() {
     return (
       <div className="footer">
-        <a href="https://github.com/Arc4d3-G" target="_blank"><img src={linkedInIcon} /></a>
-        <a href="https://www.linkedin.com/in/dewald-breed-a2297a272/" target="_blank"><img src={gitHubIcon} /></a>
+        <a href="https://github.com/Arc4d3-G" target="_blank"><img src={gitHubIcon} /></a>
+        <a href="https://www.linkedin.com/in/dewald-breed-a2297a272/" target="_blank"><img src={linkedInIcon} /></a>
       </div>
     )
   }
